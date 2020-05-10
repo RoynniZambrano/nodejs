29724c828c3742596f05243946f25c5421f23fed
Roy <roynny51@gmail.com>
2020-05-10 18:45:37 -0300

    second

---
+++ nodejs_basics/add.js
@@ -0,0 +1,12 @@
+
+
+function add(a,b){
+    return a + b;
+}
+
+
+exports.add2 = (a,b) => a + b + 1;
+
+
+
+exports.add = add;
--- nodejs_basics/first.js
+++ nodejs_basics/first.js
@@ -1,6 +1,13 @@
 
 
-console.log("hello world");
-var a = 10;
-var b = a * 3;
-console.log("b is " + b);
+var {add, add2} = require('./add.js');
+
+
+function greet(name){
+    console.log("hello there, " + name + "!");
+}
+
+
+greet("Roy");
+console.log("add:"  + add(1,2));
+console.log("add2:" + add2(1,2));
